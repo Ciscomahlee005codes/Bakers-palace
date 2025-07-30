728606d697facd556795d20861a1373e5f700596
De Boy Programmer <ciscomahlee@gmail.com>
2025-07-30 10:38:15 -0700

    Pushing Baker's Palace Project

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import StoreContextProvider from './Context/StoreContext.jsx'
 import { AuthProvider } from './Context/AuthContext.jsx' 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <AuthProvider>
       <StoreContextProvider>
         <App />
       </StoreContextProvider>
     </AuthProvider>
-  </BrowserRouter>
+  </HashRouter>
 )
